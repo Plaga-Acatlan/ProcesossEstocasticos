4733dd8529cfdb8b3134d469ff23de82bd71eba0
Sin_R0str0 <ahangel2005@gmail.com>
2026-05-10 21:42:54 -0600

    Mejoramiento de Políticas Final

--- src/Mejoramiento_Politicas.c
+++ src/Mejoramiento_Politicas.c
@@ -22,10 +22,6 @@ Matrix *Mejoramiento_Politicas(int politica_inicial){
             V->data[i] = 0;
         }
 
-        // Agregar nueva iteración a matriz de iteraciones
-        iteraciones->rows++;
-        iteraciones->data = (double**)realloc(iteraciones->data, iteraciones->rows * sizeof(double*));
-        iteraciones->data[iteraciones->rows - 1] = (double*)calloc(NUM_ESTADOS, sizeof(double));
         for(int i=0;i < NUM_ESTADOS; i++) {
             int k = V_last->data[i]-1; // Política actual para el estado i
             
@@ -51,36 +47,57 @@ Matrix *Mejoramiento_Politicas(int politica_inicial){
         gauss_jordan(A, b, solution);
         
         // Actualizar V con la solución obtenida
-        Vector *G = create_vector(1);
-        bool policy_changed = false;
         for(int i=0; i < NUM_ESTADOS; i++){
+            double value = DBL_MAX; 
+            int k_index = -1;
             for(int k=0; k < NUM_DECISIONES; k++){
-                if(policy_changed){
-                    G->size++;
-                    G->data = (double*)realloc(G->data, G->size * sizeof(double));
-                    policy_changed = false;
-                } 
                 if(g_transiciones_3d->data[k][i][0] < 0) continue; 
                 // Si hay transición desde estado i con decisión k
                 double costo_ik = g_costos->data[i][k];
                 double valor_ij = 0;
                     for(int j=0; j < NUM_ESTADOS; j++){
-                        valor_ij += g_transiciones_3d->data[k][i][j] * V_last->data[j];
+                        valor_ij += g_transiciones_3d->data[k][i][j] * solution->data[j];
                     }
-                G->data[G->size-1] = costo_ik + valor_ij - solution->data[i]; // C[i][k] + sum(P[i][j][k] * V_last[j])
+                valor_ij += costo_ik - solution->data[i]; // C[i][k] + sum(P[i][j][k] * V_last[j]) 
+                if (valor_ij<value){
+                    value = valor_ij;
+                    k_index = k+1;
+                }
             }
+            V->data[i] = k_index;
         }
-        printf("Resultados son:\n");
-        print_vector(G);
-    
-        // Verificar si hay mejora en la política
-    
         
+        /* Resultados debug:
+        log_message("LOG", "Resultados de la iteración %i", iteracion_count++);
+        print_vector(V);
+        print_vector(V_last);
+        */
+
+        iteracion_count++;
+        double **new_data = (double**)realloc(iteraciones->data, 
+                                              (iteraciones->rows + 1) * sizeof(double*));
+        if (!new_data) {
+            log_error("iteraciones", "Fallo de memoria al expandir", -1);
+            break;
+        }
+        iteraciones->data = new_data;
+        // Inicializar nueva fila
+        iteraciones->data[iteraciones->rows] = (double*)calloc(NUM_ESTADOS, sizeof(double));
+        iteraciones->rows++;
+        FOREACH_INDEX(i, NUM_ESTADOS) {
+            iteraciones->data[iteracion_count][i] = V->data[i];
+        }
 
         free_matrix(A);
         free_vector(b);
         free_vector(solution);
-        break;
     }
+    
+    FOREACH_INDEX(i, NUM_ESTADOS){
+        iteraciones->data[iteracion_count][i] = V->data[i];
+    }
+    
+    free_vector(V);
+    free_vector(V_last);
     return iteraciones;
 }
